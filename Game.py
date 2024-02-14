94fe8acdc35a0dac25e3baf468bb26c67f89e05b
chetnabarawal <159696405+chetnabarawal@users.noreply.github.com>
2024-02-14 11:49:29 +0530

    Game.py

--- Game.py
+++ Game.py
@@ -1,26 +1,30 @@
 import random
 
 
-user_choice = int(input("Enter your choice: type 0 for Rock, 1 for Paper, 2 for Scissors."))
-       
-computer_choice = random.randint(0,2)
+user_choice = int(input("Enter your choice: Type 0 for Rock, 1 for Paper, 2 for Scissors."))
 
-print("Computer Chose:")
-print(computer_choice)
+if user_choice >= 3 or user_choice < 0:
+    print("You entered a invalid number. You lose!!.")
+else:           
+    computer_choice = random.randint(0,2)
+
+    print("Computer Chose:")
+    print(computer_choice)
 
 
-if computer_choice == user_choice:
-    print ("It's a draw.")
+    if computer_choice == user_choice:
+        print ("It's a draw.")
 
-elif computer_choice > user_choice:
-    print ("You Lost !!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You Lost !!.")
 
-elif user_choice > computer_choice:           
-    print("You Win!!")     
+    elif user_choice == 0  and computer_choice == 2:
+        print("You Win !!.")   
+            
+    elif computer_choice > user_choice:
+        print ("You Lost !!.")
 
-elif computer_choice == 0 and user_choice == 2:
-    print("You Lost !!")
+    elif user_choice > computer_choice:           
+        print("You Win!!.")     
 
-elif user_choice == 0  and computer_choice == 2:
-    print("You Win !!")   
          
